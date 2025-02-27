3f7cb3f3694a848772d6c34bf582ca5c0b0ab779
Vladimir Rachkin <vova@kpnn.ru>
2025-02-27 19:25:33 +0300

    Update gpt.py

--- bot/gpt.py
+++ bot/gpt.py
@@ -1,101 +1,107 @@
-from yandex_cloud_ml_sdk import YCloudML
+# from yandex_cloud_ml_sdk import YCloudML
+from typing import List
+
+import requests
 
 
 import re
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
 YANDEX_FOLDER= os.getenv('YANDEX_CLOUD_FOLDER')
 YANDEX_TOKEN = os.getenv('YANDEX_CLOUD_TOKEN')
 
 
-sdk = YCloudML(
-    folder_id=YANDEX_FOLDER,
-    auth=YANDEX_TOKEN,
-)
+# sdk = YCloudML(
+#     folder_id=YANDEX_FOLDER,
+#     auth=YANDEX_TOKEN,
+# )
 
-#       Напоминай о дипломе
+class GPT():
 
-#       $$REMINDER_EVENT$$
+    def __init__(self):
+        pass
+
+    def run(self, messages, tools):
+
+        response = requests.post(
+            'https://llm.api.cloud.yandex.net/foundationModels/v1/completion',
+            headers={
+                'Authorization': f'Api-Key {YANDEX_TOKEN}',
+                'x-folder-id': f'{YANDEX_FOLDER}'
+            },
+            json={
+                "modelUri": f"gpt://{YANDEX_FOLDER}/yandexgpt",
+                "tools": tools,
+                "messages": messages,
+            }
+        )
+
+        return response.json()['result']['alternatives'][0]
+
+
+
 
 messages = [
-    {"role": "system", "text": },
-    #  {"role": "assistant", "text": ""},
-
+    {"role": "system", "text": " "},
+    {"role": "user", "text": "Какая погода в Москве и в Питере?"},
 ]
 
-# Примеры общения с пользователем:
-     
-#     {"role": "user", "text": "Напоминай чистить зубы перед сном"},
-#     {"role": "assistant", "text": "/new_reminder Чистить зубы перед сном"},
-#     {"role": "system", "text": "Reminder saved!"},
-#     {"role": "assistant", "text": "Я создал напоминание"},
-#     {"role": "user", "text": "Убери напоминание о чистке зубов"},
-#     {"role": "assistant", "text": "/get_reminders"},
-#     {"role": "system", "text": "Reminder list:\n 1. Чистить зубы перед сном"},
-#     {"role": "assistant", "text": "/remove_reminder 1"},
-#     {"role": "system", "text": "Reminder removed!"},
-#      {"role": "assistant", "text": "Я удалил напоминание"},
+tools = [ {
+        "function": {
+            "name": "weatherTool",
+            "description": "Получает текущую погоду в указанном городе.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "city": {
+                        "type": "string",
+                        "description": "Название города, например, Москва"
+                    }
+                },
+                "required": [
+                    "city"
+                ]
+            }
+        }
+    }
+]
 
-model = sdk.models.completions("yandexgpt").configure(temperature=0.5)
-result = model.run(messages)
-
+# model = sdk.models.completions("yandexgpt", model_version="rc").configure(temperature=0.5)
 reminders = []
 
+gpt = GPT()
+
 while True:
-    result = model.run(messages)
-    print('!assistant:', f'{result.alternatives[0].text}')
 
-    request = result.alternatives[0].text
+    request_body = {
+        "messages": messages,
+        "tools": tools,
+    }
 
+    
 
-    if request[0] == '/':
-        request = request.split('\n')[0]
+    response = gpt.run(messages, tools)
 
-        messages += [{"role": "assistant", "text": request}]
+    print (response)
 
-        respond = None
-        # Command request
-        match = re.fullmatch('/new_reminder (.*)', request)
-        if match:
-            reminders += [match[1]]
-            respond = "Reminder created!"
+    break
 
-        match = re.fullmatch('/get_reminders', request)
-        if match:
-            if len(reminders) > 0:
-                respond = "Reminder list: \n" + '\n'.join([f'{i+1}. {r}' for i, r in enumerate(reminders)])
-            else:
-                respond = "Reminder list: Empty"
+    # # result = model.run(messages)
+    # print(result)
+    # respond = result.alternatives[0].text
+    # print('!assistant:', f'{respond}')
 
-        match = re.fullmatch(r'/remove_reminder (\d+)', request)
-        if match:
-            index = int(match[1]) - 1
-            reminders = reminders[:index] + reminders[index+1:]
-            respond = "Reminder removed!"
+    # m = { "role": "assistant", "text": respond }
+    # messages += [m]
 
-        if respond is None:
-            respond = "Unknown command!"
+    # text = input("!user: ")
 
-        print('!console:', respond)
-        messages += [{"role": "user", "text": "console: " + respond}]
-            
-    else:
-        messages += [{"role": "assistant", "text": request}]
+    # if text != "":
+        
+    #     m = { "role": "user", "text": text }
 
-        user_text = input("!user: ")
-
-        if user_text == '$$REMINDER_TIME$$':
-            messages += [{"role": "user", "text": 'console: $$REMINDER_TIME$$'}]
-
-
-        if user_text == '/retry':
-            messages = messages[:-1]
-            continue
-
-        if user_text == '':
-            continue
-
-        messages += [{"role": "user", "text": user_text}]
+    #     messages += [m]
+    
